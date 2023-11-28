b8536e5185cdaf216cce99a6b8bd959c444912d6
Isaac Lyne <IsaactimothyLyne@gmail.com>
2023-11-28 22:37:02 +1300

    Update certification.service.ts

--- app/src/app/services/certification.service.ts
+++ app/src/app/services/certification.service.ts
@@ -9,7 +9,7 @@ import { Observable } from 'rxjs';
 export class CertificationService {
 
   constructor(private http: HttpClient) { }
-  private apiUrl = 'https://songstats.p.rapidapi.com/tracks/stats?source=all&isrc={REPLACETHIS}&with_playlists=true&with_charts=true&with_videos=true&with_links=true';
+  private apiUrl = 'https://https://cors-anywhere.herokuapp.com/corsdemo/https://songstats.p.rapidapi.com/tracks/stats?source=all&isrc={REPLACETHIS}&with_playlists=true&with_charts=true&with_videos=true&with_links=true';
 
   private headers = new HttpHeaders({
     'X-RapidAPI-Key': '785b386cc3msha7f1f891157a5e8p1c6690jsn058bb7feab20',
@@ -18,6 +18,10 @@ export class CertificationService {
   
   fetchCertification(isrc: string): Observable<any> {
     const newUrl = this.apiUrl.replace('{REPLACETHIS}', isrc);
-    return this.http.get(newUrl, { headers: this.headers });
+    const requestOptions = {
+      headers: this.headers,
+      mode: 'cors', // Add this line
+    };
+    return this.http.get(newUrl, requestOptions);
   }
 }
